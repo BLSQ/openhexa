dcb37d7636285dbaefda2a50b7a02f58bfbfed1b
Quentin Gérôme <quentin@letsbuildit.dev>
2025-05-06 15:42:02 +0200

    Remove the '+' from the secret_key generation

--- script/setup.sh
+++ script/setup.sh
@@ -363,7 +363,7 @@ function generate_django_secret_key() {
   # see https://github.com/django/django/blob/07a4d23283586bc4578eb9c82a7ad14af3724057/django/core/management/utils.py#L79
   # for implementation
   # $ has been removed to avoid bash substitution
-  head -c 8192 /dev/urandom | LC_ALL=C tr -dc 'abcdefghijklmnopqrstuvwxyz0123456789!@#%^&*(-_=+)$' | head -c 50
+  head -c 8192 /dev/urandom | LC_ALL=C tr -dc 'abcdefghijklmnopqrstuvwxyz0123456789!@#%^&*(-_=)$' | head -c 50
 }
 
 function generate_fernet_encryption_key() {
