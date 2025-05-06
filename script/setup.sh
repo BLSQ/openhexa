7ec20c84397537900fde67843bb08dcbf6b0bf2a
Quentin Gérôme <quentin@letsbuildit.dev>
2025-05-06 15:33:19 +0200

    feat: Update app, frontend, jupyterhub images

--- script/setup.sh
+++ script/setup.sh
@@ -343,8 +343,8 @@ OPENHEXA_GROUP=openhexa
 
 function setup_user() {
   if [[ $OPTION_GLOBAL == "on" ]]; then
-    $SUDO_COMMAND addgroup --system "${OPENHEXA_GROUP}"
-    $SUDO_COMMAND adduser --system --ingroup "${OPENHEXA_GROUP}" "${OPENHEXA_USER}"
+    $SUDO_COMMAND addgroup --system --gid 1000 "${OPENHEXA_GROUP}"
+    $SUDO_COMMAND adduser --system --uid 1000 --ingroup "${OPENHEXA_GROUP}" "${OPENHEXA_USER}"
     $SUDO_COMMAND usermod -a -G docker "${OPENHEXA_USER}"
   fi
 }
@@ -352,7 +352,7 @@ function setup_user() {
 function setup_local_storage() {
   if [[ $OPTION_GLOBAL == "on" ]]; then
     $SUDO_COMMAND mkdir -p "${WORKSPACE_DATA_DIRECTORY}"
-    $SUDO_COMMAND chown "${OPENHEXA_USER}:${OPENHEXA_GROUP}" "${WORKSPACE_DATA_DIRECTORY}"
+    $SUDO_COMMAND chown 1000:1000 "${WORKSPACE_DATA_DIRECTORY}"
   else
     mkdir -p "${WORKSPACE_DATA_DIRECTORY}"
     chmod 777 "${WORKSPACE_DATA_DIRECTORY}"
